c4b6f405078f65ca0474abaf0969930fe2e40491
Bernard Lee <bernardlee82@gmail.com>
2015-08-23 17:22:24 -0400

    Enable strict mode and reformat code.
    Add "use strict" directive to each JavaScript file and convert all
    spaces to tabs.

--- models/user.js
+++ models/user.js
@@ -2,58 +2,57 @@
 
 var mongoose = require('mongoose');
 
-var userSchema = new mongoose.Schema ({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: false,
-    unique: true,
-    validates: {
-      isEmail: true
-    }
-  },
-  password : {
-    type : String,
-    required : false,
-    unique : false
-  },
-  dob: {
-    type: String,
-    match: /\d{4}-\d{2}-\d{2}/
-  },
-  phone_number: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  // Address Book Part
-  address_book: {
-    primary_address: {
-      name: {
-        last: {
-          type: String,
-          required: true
-        },
-        first: {
-          type: String,
-          required: true
-        }
-      },
-      address : {
-        type: String,
-        required: false
-      }
-    }
-  }
+var userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	email: {
+		type: String,
+		required: false,
+		unique: true,
+		validates: {
+			isEmail: true
+		}
+	},
+	password: {
+		type: String,
+		required: false,
+		unique: false
+	},
+	dob: {
+		type: String,
+		match: /\d{4}-\d{2}-\d{2}/
+	},
+	phone_number: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	// Address Book Part
+	address_book: {
+		primary_address: {
+			name: {
+				last: {
+					type: String,
+					required: true
+				},
+				first: {
+					type: String,
+					required: true
+				}
+			},
+			address: {
+				type: String,
+				required: false
+			}
+		}
+	}
 }, {
-  timestamps: true,
+	timestamps: true,
 });
 
 var User = mongoose.model('User', userSchema);
 
 module.exports = User;
-
