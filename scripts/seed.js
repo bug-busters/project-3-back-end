b8d5dad9a3d2001c76763faa1d815e3cb6bcf4d8
lekova <lina.lekova@gmail.com>
2015-08-23 16:11:38 -0400

    Create seed.js for with sample user to be inserted in mongodb

---
+++ scripts/seed.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var db = new Mongo().getDB('syntactic_sugar');
+db.users.remove({});
+
+db.users.insert({
+   username: 'user1',
+   email: 'user1@test.com',
+   password: 'idontknow',
+   dob: '2015-08-23',
+   phone_number: '123-456-7890',
+   address_book: {
+    primary_address: {
+      name: {
+        last: 'Doe',
+        first: 'John'
+      },
+      address: '12345 Main Str, Boston, MA'
+    }
+  }
+});
