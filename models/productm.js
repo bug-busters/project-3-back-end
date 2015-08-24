9b76c5bd21c1d9ad5c592f9a352d97a97f0ac987
Bernard Lee <bernardlee82@gmail.com>
2015-08-24 10:19:36 -0400

    Rename product.js to productm.js.
    Refactor product collection into its own file to be exported into
    index.js

---
+++ models/productm.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = function(mongoose) {
+	var Schema = mongoose.Schema;
+
+	var productSchema = new Schema({
+		sku: {
+			type: Number,
+			required: true,
+			unique: true,
+			validate: {
+				isInt: true
+			}
+		},
+		title: {
+			type: String,
+			required: true,
+			unique: true
+		},
+		description: {
+			type: String,
+			required: true
+		},
+		price: {
+			type: Number,
+			required: true,
+			validate: {
+				isDecimal: true
+			}
+		},
+		image: {
+			type: String,
+			validate: {
+				isURL: true
+			}
+		}
+	}, {
+		timestamps: true
+	});
+
+	var Product = mongoose.model('Product', productSchema);
+
+	return Product;
+};
