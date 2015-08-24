b4c86bf9d9e29ac27045c6db7a3037cf2bbd18db
Bernard Lee <bernardlee82@gmail.com>
2015-08-24 08:44:09 -0400

    Cast database port to a number.
    Add a unary "+" operator to process.env.DB_PORT to cast it into a
    number.

--- models/psql-models.js
+++ models/psql-models.js
@@ -9,9 +9,9 @@ var database = {
 	username: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	info: {
-		host:    process.env.DB_HOST,
-		port:    process.env.DB_PORT,
-		dialect: process.env.DB_DIALECT
+		host:     process.env.DB_HOST,
+		port:    +process.env.DB_PORT,
+		dialect:  process.env.DB_DIALECT
 	}
 };
 
