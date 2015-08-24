2376f07132c346922d9bf9039060d8fec7b4e64f
tiffboors29 <tiffanyboorsma@gmail.com>
2015-08-24 17:14:49 -0400

    Fixed and ran seed file

--- scripts/seed.js
+++ scripts/seed.js
@@ -26,68 +26,68 @@ var models = require('../models');
 
 async.series([
   function (cb){ //Clear DB
-    models.Product.destroy({where: {}}).then(cb);
+    models.Product.remove({where: {}}).then(cb);
   },
   function (cb){ //Seed DB
     async.parallel([
       function (cb){
         models.Product.create({
           sku: 170432,
-          title: "Vanilla Javascript",
-          description: "A simple white cake with vanilla butter cream frosting.",
+          title: 'Vanilla Javascript',
+          description: 'A simple white cake with vanilla butter cream frosting.',
           price: 2.25,
           stock: 25,
-          image: "img/2.jpg"
+          image: 'img/2.jpg'
           }).then(done);
       },
       function (cb){
         models.Product.create({
           sku: 921079,
-          title: "Chocolate 401",
-          description: "A simple white cake with vanilla butter cream frosting.",
+          title: 'Chocolate 401',
+          description: 'A simple white cake with vanilla butter cream frosting.',
           price: 2.50,
           stock: 22,
-          image: "img/3.jpg"
+          image: 'img/3.jpg'
           }).then(done);
       },
       function (cb){
         models.Product.create({
           sku: 129943,
-          title: "Ruby on Chocolate",
-          description: "Raspberry filled chocolate cake with raspberry cream cheese frosting.",
+          title: 'Ruby on Chocolate',
+          description: 'Raspberry filled chocolate cake with raspberry cream cheese frosting.',
           price: 2.75,
           stock: 6,
-          image: "img/4.jpg"
+          image: 'img/4.jpg'
           }).then(done);
       },
       function (cb){
         models.Product.create({
           sku: 444927,
-          title: "Red Velvet Gem",
-          description: "Rich red velvet cake with cream cheese frosting.",
+          title: 'Red Velvet Gem',
+          description: 'Rich red velvet cake with cream cheese frosting.',
           price: 2.25,
           stock: 3,
-          image: "img/5.jpg"
+          image: 'img/5.jpg'
           }).then(done);
       },
       function (cb){
         models.Product.create({
           sku: 308954,
-          title: "Code Confetti",
-          description: "Zesty lemon cake with vanilla butter cream frosting and sugar confetti garnish.",
+          title: 'Code Confetti',
+          description: 'Zesty lemon cake with vanilla butter cream frosting and sugar confetti garnish.',
           price: 2.50,
           stock: 13,
-          image: "img/6.jpg"
+          image: 'img/6.jpg'
           }).then(done);
       },
       function (cb){
         models.Product.create({
           sku: 654891,
-          title: "Debug Under Stars",
-          description: "Extra-dark caffeinated chocolate cake to keep you up when programming late into the night.",
+          title: 'Debug Under Stars',
+          description: 'Extra-dark caffeinated chocolate cake to keep you up when programming late into the night.',
           price: 2.75,
           stock: 13,
-          image: "img/7.jpg"
+          image: 'img/7.jpg'
           }).then(done);
       }
     ], function (err, results){
@@ -99,5 +99,5 @@ async.series([
     });
   }
 ], function (err, results){
-  console.log("DB is ready");
+  console.log('DB is ready');
 });
