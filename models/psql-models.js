285d708ffa45bd342e2f584dfe9d3c7fe2a81920
Bernard Lee <bernardlee82@gmail.com>
2015-08-23 22:06:52 -0400

    Add psql-models.js file for Sequelize and PostgreSQL.
    Add a Sequelize script to model products using a table with columns
    for id and SKU.

---
+++ models/psql-models.js
@@ -0,0 +1,37 @@
+'use strict';
+
+require('dotenv').load();
+
+var Sequelize = require('sequelize');
+
+var database = {
+	name:     process.env.DB_NAME,
+	username: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	info: {
+		host:    process.env.DB_HOST,
+		port:    process.env.DB_PORT,
+		dialect: process.env.DB_DIALECT
+	}
+};
+
+var sequelize = new Sequelize(database.name, database.username, database.password, database.info);
+
+var Product = sequelize.define('Product', {
+	id: {
+		type: Sequelize.INTEGER.UNSIGNED,
+		autoIncrement: true,
+		primaryKey: true,
+		allowNull: false
+	},
+	sku: {
+		type: Sequelize.INTEGER.UNSIGNED,
+		allowNull: false
+	}
+});
+
+var models = {
+	Product: Product
+};
+
+module.exports = models;
