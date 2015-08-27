37a1484f62ffeb5f3bd0d23d66d9a2269b612283
joebegley15 <joebegley15@gmail.com>
2015-08-27 19:25:58 -0400

    Added a post route for past orders.

--- routes/past-orders.js
+++ routes/past-orders.js
@@ -15,7 +15,24 @@ router.route('/:user_id')
           grandTotal: 0
         }
       }
-    };
+    }
+    .post(function(req, res) {
+      // Create past order
+        console.log('post /past_orders/user_id');
+        console.log(req.body);
+        models.Past-Orders.create({
+            'user_id': req.params.user_id,
+            'products': req.body.products
+        })
+        .then(function(cart) {
+          res.json(pastOrder);
+                console.log('New past order.');
+          },
+          function(error) {
+            console.log(error);
+            console.log('Failed to create a new past order.');
+        });
+    });
 
     models.PastOrder.find({
         'userId': req.params.user_id
