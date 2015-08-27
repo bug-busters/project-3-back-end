4b8fb83b870dfad2997c3f11a1f652fd9c846098
joebegley15 <joebegley15@gmail.com>
2015-08-27 18:01:21 -0400

    Resolved merge conflicts and removed unneeded modules for the cart model

--- models/cart.js
+++ models/cart.js
@@ -15,28 +15,8 @@ module.exports = function(sequelize, DataTypes) {
 		},
 		products: {
 			type: DataTypes.JSON,
-<<<<<<< HEAD
 		},
-		secret_key: process.env.STRIPE_TEST_SECRET_KET
-	}, {
-		classMethods: {
-			getSubtotals: function() {
-				var subtotals = [];
-				this.products.forEach(function(product) {
-					subtotals.push(product.price * product.quantity);
-				});
-
-				return subtotals;
-			},
-
-			getTotal: function() {
-				return (this.getSubtotals().reduce(function(a, b) {
-					return a + b;
-				}, 0)).toFixed(2);
-			}
-=======
->>>>>>> beacfdc2745cfb072310ab7aeb3451d2351780a3
-		}
+		secret_key: process.env.STRIPE_TEST_SECRET_KEY
 	});
 
 	return Cart;
