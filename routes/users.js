e1135b3fd6c72db547811f677cec856c0dc7ffb1
tiffboors29 <tiffanyboorsma@gmail.com>
2015-08-28 09:47:22 -0400

    Fix get route for users and add delete route

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,37 @@
 
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	res.send('respond with a resource');
-});
+router.route('/:id')
+  .get(function(req, res) {
+    console.log('inside get for users');
+  	models.User.find({
+      where: {
+        'id': req.params.id
+      }
+    })
+    .then(function(user) {
+        res.status(200).json(user);
+    }, function(error) {
+        console.error(error);
+    });
+  })
+  .delete(function(req, res) {
+    models.User.destroy({
+      where: {
+        'id': req.params.id
+      }
+    })
+    .then(function() {
+        res.send('Your account has been deleted.');
+    }, function(error) {
+        console.error(error);
+    });
+  });
+
+
+
 
 module.exports = router;
