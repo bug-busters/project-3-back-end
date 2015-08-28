5850898cc07aa6a2b8b6f4a3d44b115f5a81bcdb
Bernard Lee <bernardlee82@gmail.com>
2015-08-28 09:45:46 -0400

    Add GET and POST routes for /past-orders/:user_id.
    Add basic GET and POST routes for /past-orders/:user_id. These routes
    have not been tested yet and will most likely require changes.

--- routes/past-orders.js
+++ routes/past-orders.js
@@ -6,17 +6,38 @@ var models = require('../models/index');
 
 /* GET past orders of user*/
 router.route('/:user_id')
-  .get(function (req, res) {
-    //Carts index
-    models.PastOrder.find({
-        'userId': req.params.user_id
-      }).then(function(pastorder) {
-          // var pastordersJSON = JSON.parse(pastorder.products);
-          res.json(pastorder);
-        },
-        function(error) {
-          console.log(error);
-        });
-    });
+	.get(function(req, res) {
+		var pastOrders = [];
+
+		models.PastOrder.find({
+				'userId': req.params.user_id
+			})
+			.then(function(pastOrder) {
+					pastOrders.push(JSON.parse(pastOrder));
+					res.status(200).json(pastOrders);
+				},
+				function(error) {
+					console.error('GET /past-orders/:user_id error');
+					console.error(error);
+					res.sendStatus(500);
+				});
+	})
+	.post(function(req, res) {
+		models.PastOrder.create({
+			userId: null,
+			orderDate: null,
+			products: [],
+			status: null,
+			subTotals: this.subtotal(),
+			grandTotal: this.grandTotal()
+		})
+			.then(function() {
+				console.log('success');
+				res.sendStatus(200);
+			}, function() {
+				console.error('past order create failed');
+				res.sendStatus(500);
+			});
+	});
 
 module.exports = router;
