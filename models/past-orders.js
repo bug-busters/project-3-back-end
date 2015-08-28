59b57467682a319de94d81db6c96e2c58785fdd6
Bernard Lee <bernardlee82@gmail.com>
2015-08-28 09:48:13 -0400

    Rename total() to grandTotal().
    Rename the virtual method total() to grandTotal().

--- models/past-orders.js
+++ models/past-orders.js
@@ -1,82 +1,77 @@
 'use strict';
 
 module.exports = function(mongoose) {
-  var Schema = mongoose.Schema;
+	var Schema = mongoose.Schema;
 
-  var pastOrderSchema = new Schema(
-    {
-    userId: {
-      type: Number,
-      required: true,
-      validates: {
-        isInt: true
-      }
-    },
-    orderDate: {
-      type: Date,
-      default: Date.now
-    },
-    products: [
-      {
-        sku: {
-          type: Number,
-          required: true,
-          unique: true,
-          validates: {
-            isInt: true
-          }
-        },
-        title: {
-          type: String,
-          required: true,
-          unique: true
-        },
-        price: {
-          type: Number,
-          required: true,
-          validates: {
-            isDecimal: true
-          }
-        },
-        quantity: {
-          type: Number,
-          required: true,
-          unique: true,
-          validates: {
-            isInt: true
-          }
-        }
-      }
-    ],
-    status: {
-      type: String,
-      required: true
-    }
-  });
+	var pastOrderSchema = new Schema({
+		userId: {
+			type: Number,
+			required: true,
+			validates: {
+				isInt: true
+			}
+		},
+		orderDate: {
+			type: Date,
+			default: Date.now
+		},
+		products: [{
+			sku: {
+				type: Number,
+				required: true,
+				unique: true,
+				validates: {
+					isInt: true
+				}
+			},
+			title: {
+				type: String,
+				required: true,
+				unique: true
+			},
+			price: {
+				type: Number,
+				required: true,
+				validates: {
+					isDecimal: true
+				}
+			},
+			quantity: {
+				type: Number,
+				required: true,
+				unique: true,
+				validates: {
+					isInt: true
+				}
+			}
+		}],
+		status: {
+			type: String,
+			required: true
+		}
+	});
 
+	pastOrderSchema.virtual('grandTotal').get(function() {
+		var total = 0.00;
 
-  pastOrderSchema.virtual('total').get(function() {
-    var total = 0.00;
+		this.products.forEach(function(product) {
+			total += product.price * product.quantity;
+		});
 
-    this.products.forEach(function(product) {
-        total += product.price * product.quantity;
-    });
+		return total;
+	});
 
-    return total;
-  });
+	pastOrderSchema.virtual('subtotal').get(function() {
+		var subTotalArr = {};
 
-  pastOrderSchema.virtual('subtotal').get(function() {
-    var subTotalArr = {};
+		this.products.forEach(function(product) {
+			subTotalArr[product.sku] = product.price * product.quantity;
+		});
 
-    this.products.forEach(function(product) {
-        subTotalArr[product.sku] = product.price * product.quantity;
-    });
+		return subTotalArr;
+	});
 
-    return subTotalArr;
-  });
+	var PastOrder = mongoose.model('PastOrder', pastOrderSchema);
 
-  var PastOrder = mongoose.model('PastOrder', pastOrderSchema);
-
-  return PastOrder;
+	return PastOrder;
 };
-
