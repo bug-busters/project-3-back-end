3a269afbbdfa753b1cc8b2cd2bd9943dbacde2ac
Bernard Lee <bernardlee82@gmail.com>
2015-08-28 12:13:54 -0400

    Create routes for checking out.
    Create checkout routes for communication between the front end and the
    Stripe API.

---
+++ routes/checkout.js
@@ -0,0 +1,32 @@
+'use strict';
+
+require('dotenv').load();
+var express = require('express');
+var router = express.Router();
+var stripe = require('stripe')(process.env.STRIPE_TEST_SECRET_KEY);
+
+router.route('/')
+	.get(function(req, res) {
+		console.log('get checkout');
+		res.sendStatus(403);
+	})
+	.post(function(req, res) {
+		console.log('post checkout');
+		stripe.charges.create(req.body, function(error, response) {
+			if (error) {
+				console.error('stripe charge error');
+				console.error(error);
+			}
+
+			if (response && response.status === 'succeeded') {
+				console.log('stripe charge successful!');
+				res.status(200).json({ 'status': 'succeeded' });
+			} else {
+				console.log('stripe charge failed!');
+				res.status(403).json({ 'status': 'failed' });
+			}
+			// res.sendStatus(403);
+		});
+	});
+
+module.exports = router;
