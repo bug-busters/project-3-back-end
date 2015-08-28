204b28e7380fbe0c5eb2780f9957246b30739605
tiffboors29 <tiffanyboorsma@gmail.com>
2015-08-27 21:16:40 -0400

    Fix get route and return formatted json object for better handlebar manipulation

--- routes/past-orders.js
+++ routes/past-orders.js
@@ -7,41 +7,59 @@ var models = require('../models/index');
 /* GET past orders of user*/
 router.route('/:user_id')
   .get(function (req, res) {
-    var outGoingHistory = {
-      date: {
-        products: {},
-        totals: {
-          subtotals: {},
-          grandTotal: 0
-        }
+
+
+
+
+      var outGoingHistory = [];
+
+        // oderDate: ,
+        // grandTotal: ,
+        // products: [{
+        //   title: ,
+        //   quantity: ,
+        //   price: ,
+        //   subtotal: ,
+        // }],
+        // status: ,
+    models.PastOrder.find({
+      'user_id': req.params.user_id
+    })
+    .then(function (pastorder) {
+
+      for (var i = 0; i < pastorder.length; i++) {
+        var pastOrder = {
+          products: {},
+        };
+        pastOrder['status'] = pastorder[i].status;
+        pastOrder['products'] = pastorder[i].products;
+        pastOrder['grandTotal'] = pastorder[i].grandTotal;
+        pastOrder['orderDate'] = pastorder[i].orderDate;
+        outGoingHistory.push(pastOrder);
       }
-    }
-    .post(function(req, res) {
-      // Create past order
-        console.log('post /past_orders/user_id');
-        console.log(req.body);
-        models.Past-Orders.create({
-            'user_id': req.params.user_id,
-            'products': req.body.products
-        })
-        .then(function(cart) {
-          res.json(pastOrder);
-                console.log('New past order.');
-          },
-          function(error) {
-            console.log(error);
-            console.log('Failed to create a new past order.');
-        });
+
+      console.log('outGoingHistory: ', outGoingHistory);
+      res.json(outGoingHistory);
+    },
+    function (error) {
+      console.log(error);
+    });
+  })
+  .post(function (req, res) {
+    // Create past order
+    console.log('post /pastorders/user_id');
+    console.log(req.body);
+
+    models.PastOrder.create(req.body)
+      .then(function(pastorder) {
+        res.json(pastorder);
+        console.log('New past order created.');
+    },
+    function(error) {
+      console.log(error);
+      console.log('Failed to create a new past order.');
     });
 
-    models.PastOrder.find({
-        'userId': req.params.user_id
-      }).then(function(pastorder) {
-          res.json(pastorder);
-        },
-        function(error) {
-          console.log(error);
-        });
-    });
+  });
 
 module.exports = router;
