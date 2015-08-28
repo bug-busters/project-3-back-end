eb045f92edc74b69697c9051f49a1d322c9209df
Bernard Lee <bernardlee82@gmail.com>
2015-08-28 11:02:29 -0400

    Modify GET request and add POST request for /past-orders/:user_id.
    Add POST request handler for router and modify GET request handler to
    respond with HTTP statuses.

--- routes/past-orders.js
+++ routes/past-orders.js
@@ -6,60 +6,62 @@ var models = require('../models/index');
 
 /* GET past orders of user*/
 router.route('/:user_id')
-  .get(function (req, res) {
+	.get(function(req, res) {
+		var outGoingHistory = [];
 
+		// oderDate: ,
+		// grandTotal: ,
+		// products: [{
+		//   title: ,
+		//   quantity: ,
+		//   price: ,
+		//   subtotal: ,
+		// }],
+		// status: ,
+		models.PastOrder.find({
+				'user_id': req.params.user_id
+			})
+			.then(function(pastorder) {
+					for (var i = 0; i < pastorder.length; i++) {
+						var pastOrder = {
+							products: {},
+						};
+						pastOrder.status = pastorder[i].status;
+						pastOrder.products = pastorder[i].products;
+						pastOrder.grandTotal = pastorder[i].grandTotal;
+						pastOrder.orderDate = pastorder[i].orderDate;
+						outGoingHistory.push(pastOrder);
+					}
 
+					console.log('outGoingHistory: ', outGoingHistory);
+					res.status(200).json(outGoingHistory);
+				},
+				function(error) {
+					console.error('error finding past orders');
+					console.error(error);
+					res.sendStatus(200);
+				});
 
-
-      var outGoingHistory = [];
-
-        // oderDate: ,
-        // grandTotal: ,
-        // products: [{
-        //   title: ,
-        //   quantity: ,
-        //   price: ,
-        //   subtotal: ,
-        // }],
-        // status: ,
-    models.PastOrder.find({
-      'user_id': req.params.user_id
-    })
-    .then(function (pastorder) {
-
-      for (var i = 0; i < pastorder.length; i++) {
-        var pastOrder = {
-          products: {},
-        };
-        pastOrder['status'] = pastorder[i].status;
-        pastOrder['products'] = pastorder[i].products;
-        pastOrder['grandTotal'] = pastorder[i].grandTotal;
-        pastOrder['orderDate'] = pastorder[i].orderDate;
-        outGoingHistory.push(pastOrder);
-      }
-
-      console.log('outGoingHistory: ', outGoingHistory);
-      res.json(outGoingHistory);
-    },
-    function (error) {
-      console.log(error);
-    });
-  })
-  .post(function (req, res) {
-    // Create past order
-    console.log('post /pastorders/user_id');
-    console.log(req.body);
-
-    models.PastOrder.create(req.body)
-      .then(function(pastorder) {
-        res.json(pastorder);
-        console.log('New past order created.');
-    },
-    function(error) {
-      console.log(error);
-      console.log('Failed to create a new past order.');
-    });
-
-  });
+	})
+	.post(function(req, res) {
+		console.log(req.body);
+		models.PastOrder.create({
+				userId: null,
+				orderDate: null,
+				products: [],
+				status: null,
+				subTotals: this.subtotal(),
+				grandTotal: this.grandTotal()
+			})
+			.then(function(aaa) {
+				console.log('success');
+				console.log(aaa);
+				res.sendStatus(200);
+			}, function(error) {
+				console.error('POST /past-orders/:user_id error');
+				console.error(error);
+				res.sendStatus(500);
+			});
+	});
 
 module.exports = router;
