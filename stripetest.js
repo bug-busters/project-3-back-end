6136e41ccac94f0172a93ef68ec6732dcc63906d
Bernard Lee <bernardlee82@gmail.com>
2015-08-28 09:51:24 -0400

    Add Stripe API test file.
    Create a JavaScript file for testing connections to the Stripe API. The
    file creates a "charge" object with the specifications in the Stripe
    API documentation and then sends it. Run with "node stripetest.js" in
    the CLI.

---
+++ stripetest.js
@@ -0,0 +1,29 @@
+'use strict';
+
+require('dotenv').load();
+
+var stripe = require('stripe')('process.env.STRIPE_TEST_SECRET_KEY');
+
+var token = {
+	number: 4242424242424242,
+	cvc: 123,
+	exp_month: 1,
+	exp_year: 2016
+};
+
+var charge = {
+	amount: 42069,
+	currency: 'usd',
+	source: token,
+	description: 'test charge'
+};
+
+stripe.charges.create(charge, function(error, charge) {
+	if (error) {
+		console.error('error');
+		console.error(error);
+	}
+
+	console.log('charge');
+	console.log(charge);
+});
